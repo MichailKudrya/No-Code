5eea523dc3f849fb1281ef9bdb324227a3a19acf
MichailKudrya <miget221@gmail.com>
2022-12-28 22:55:00 +0200

    create carousel

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ function navToggle() {
   var body = document.getElementById("body");
   body.classList.toggle("_overflow");
 }
+
+
+function toggleThing(id) {
+  var thing = document.getElementById(id);
+  thing.classList.toggle("_active");
+}
+ 
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
